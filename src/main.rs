f4d879b5e8ae6448bfa1fd12cdfcdfe94939a582
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2021-01-04 14:26:43 -0300

    Add some password generating logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,49 @@
+use rand::{self, Rng};
+use shannon_entropy::shannon_entropy;
+use std::fmt;
 use clap::{App, AppSettings, Arg}; /* For command line arguments */
 
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                         abcdefghijklmnopqrstuvwxyz";
+const PASSWORD_LEN: usize = 20;
+
+#[derive(Eq, PartialEq)]
+enum PasswordQuality {
+    Bad,
+    Weak,
+    Good,
+    Excellent,
+}
+
+impl fmt::Display for PasswordQuality {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let txt = match self {
+            PasswordQuality::Bad => "Bad",
+            PasswordQuality::Weak => "Weak",
+            PasswordQuality::Good => "Good",
+            PasswordQuality::Excellent => "Excellent"
+        };
+        write!(f, "{}", txt)
+    }
+}
+
+// I'm guesstimating here
+fn password_quality(shannon_entropy: f32) -> PasswordQuality {
+    let x = shannon_entropy;
+    match x  {
+        _ if x <= 2.5 => PasswordQuality::Bad,
+        _ if x <= 3.3 => PasswordQuality::Weak,
+        _ if x <= 3.7 => PasswordQuality::Good,
+        _             => PasswordQuality::Excellent,
+    }
+}
+
+
 fn main() {
-    let matches = App::new("Shrimp")
+    let _matches = App::new("Shrimp")
         .version("0.1")
-        .author("Vini And Merazi")
-        .about("Rusty password generator.")
+        .author("github.com/carmesim/shrimp")
+        .about("Simple password generator in Rust.")
         .settings(&[AppSettings::ColoredHelp])
         .arg(
             Arg::with_name("uppercase")
@@ -14,9 +53,9 @@ fn main() {
         )
         .arg(
             Arg::with_name("lowercase")
-                .long("--nolow")
+                .long("--lowercase")
                 .short("-l")
-                .help("Enable lowercase letters. (Enabled by default)"),
+                .help("Enable lowercase letters."),
         )
         .arg(
             Arg::with_name("symbols")
@@ -25,18 +64,37 @@ fn main() {
                 .help("Enable symbols like #, $ or @."),
         )
         .arg(
+            Arg::with_name("silence")
+                .long("--silence")
+                .short("-S")
+                .help("Suppress all output bar the generated password."),
+        )
+        .arg(
             Arg::with_name("numbers")
                 .long("--numbers")
                 .short("-n")
                 .help("Enable whole numbers like 1, 2, or 8."),
         )
         .arg(
-            Arg::with_name("language")
-                .long("--lang")
-                .short("-L")
-                .value_name("Language")
-                .help("Set a base string to take the characters from."),
+            Arg::with_name("pool")
+                .long("--pool")
+                .short("-p")
+                .value_name("POOL")
+                .help("Sets the pool of characters from which the password can be made of."),
         )
         .get_matches();
-    println!("Hello, world!");
+
+        let mut rng = rand::thread_rng();
+
+        let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+        let pw_quality = password_quality(shannon_entropy(&password));
+
+        println!("Password: {}\nQuality:{}", password, pw_quality);
+        
 }
